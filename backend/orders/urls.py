b5d65a98a6a552fb331ef2eeaa156ffe0a9cd5ca
Watit Thammarat <tongnakub@hotmail.com>
2018-05-29 21:09:52 +0700

    add refresh token endpoint

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
+from rest_framework_jwt.views import refresh_jwt_token
 from orders.views import (sigin, sigup, make_order,
                           get_order, MenuList, MenuDetail, get_order_summary)
 
 urlpatterns = [
     url(r'^sign-in$', sigin),
     url(r'^sign-up$', sigup),
+    url(r'^token/refresh$', refresh_jwt_token),
     url(r'^orders$', make_order),
     url(r'^orders/(?P<timestamp>[0-9]+)/summary$', get_order_summary),
     url(r'^orders/(?P<timestamp>[0-9]+)$', get_order),
